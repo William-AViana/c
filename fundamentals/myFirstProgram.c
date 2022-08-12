df0d971ea2d2c69bbcaf40da7735294413f44d36
William Viana <willdev.viana@gmail.com>
2022-08-11 21:43:50 -0300

    my first program in c with standard I/O

---
+++ fundamentals/myFirstProgram.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // biblioteca com funções de entrada e saída
+
+int main() {
+  printf("Hello world!\n"); //funcção usada para saída de valores/texto
+  printf("I am learning C.");
+  return 0;
+}
