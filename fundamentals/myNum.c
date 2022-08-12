e3a448465783bca0c1be1444415f3952efad748e
William Viana <willdev.viana@gmail.com>
2022-08-11 21:44:35 -0300

    my first program with variables

---
+++ fundamentals/myNum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int myNum = 20;
+  float myFloatNum = 9.99;
+  char myLeter = 'W';
+
+  printf("%d\n", myNum);
+  printf("%f\n", myFloatNum);
+  printf("%c\n", myLeter);
+}
