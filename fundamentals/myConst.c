7495d275686f9cbca48ca3a60c6e58e28ccf016b
William Viana <willdev.viana@gmail.com>
2022-08-13 19:18:27 -0300

    undrstand const with C

---
+++ fundamentals/myConst.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  const int BIRTHYER = 1972;
+  // Cannot assign values ​​to a constant
+  // BIRTHYER = 1991; 
+
+  printf("The year of birth is %i", BIRTHYER);
+}
